106ab261eb4ab801dae4177eaa3229beeb866372
Matt <123321mail@gmail.com>
2019-03-29 12:40:41 -0500

    tons of changes
    big shout out to hfuller for letting me steal tons of code, this is just being uploaded to save it incase pc dies, still in progress

--- src/NFC-WiFi.ino
+++ src/NFC-WiFi.ino
@@ -60,6 +60,7 @@ const uint8_t dataPin = 5;
 #define PN532_MOSI (12)
 #define PN532_SS   (13)
 #define PN532_MISO (14)
+#define VERSION      1
 
 
 
@@ -79,34 +80,41 @@ const uint16_t ledCount = 1;
 const uint8_t brightness = 1;
 
 String notice;
-
-  
-const char MAIN_page1[]PROGMEM=R"=====( <!DOCTYPE html><html>
-  <head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-  <link rel=\"icon\" href=\"data:,\">
-  <style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
-  .button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;
-  text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2 {background-color: #77878A;}</style></head>
-  <body><h1>NFC WiFi </h1>
-  <p>Click register button and then place NFC tag over reader</p>
-  <form action="/uid1rec" method="get">
-    <button type="submit">Register</button><br></form>
-  <p>Tag1 = ")=====";
-
-const char MAIN_page2[]PROGMEM=R"=====("</p>
-  <p>URL1 = ")=====";
+File fsUploadFile;              // a File object to temporarily store the received file
 
 
-const char MAIN_page3[]PROGMEM=R"=====("</p>
-  <FORM METHOD="POST"action="/submitURL1">
-    <input type="text" name="myText" value="Tag1 URL...">
-  <input type="submit" value="Submit">
-  </form>
-  <p>Once you've registered the tag and enter a URL hit Run and the reader will be working</p>
-  <form action="/run" method="get">
-    <button type="submit">Run</button><br>  
-  </body></html>)=====";
+int currentEffectId = 2;
+struct EffectState {
+  String color;
+  String nextColor;
+  bool boolEffectState;
+  int intEffectState;
+  int intEffectState2;
+  int intEffectState3;
+  unsigned long ulongEffectState;
+  unsigned long ulongEffectState2;
+  String currentPalette;
+ // bool lowPowerMode;
+};
+
+const char * header = R"(<!DOCTYPE html>
+<html>
+<head>
+<title>NFC-WiFi</title>
+<link rel="stylesheet" href="/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+</head>
+<body>
+<div id="top">
+  <span id="title">NFC-WiFi</span>
+  <a href="/">Controls</a>
+  <a href="/setup">Setup</a>
+  <a href="/debug">Debug</a>
+  <a href="/update">Update</a>
+</div>
+)";
+
+
 
 String WebPage =""; 
 
@@ -116,12 +124,12 @@ void LED_Off();
 void LED_Blue();
 void LED_Red();
 void LED_Green();
-void url1submit();
 void uid1record();
 void UseURL1(String);
-void UpdateWebPage();
 void nfcread();
 void setClock();
+void send302(String);
+
 void setup()
 { 
   Serial.begin(115200);
@@ -157,8 +165,259 @@ void setup()
   nfc.SAMConfig();
   LED_Off();
   //server.on("/",handlePostForm);
-  WebPage= MAIN_page1+uid1str+MAIN_page2+url1str+MAIN_page3;
 
+server.on("/update", HTTP_GET, [&](){
+    String content = header;
+    content += ("<h1>Update</h1>");
+
+ 
+    content += R"(
+      <h2>Update cert from a file</h2>
+      <p>Run this python script, and upload the cert.ar file if need be(or you can upload your own if you want)</p>
+      <p>https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/BearSSL_CertStore/certs-from-mozilla.py</p>
+      <p>This shouldn't be required, but you never know. If using your own use cert.ar filename</p>
+      <form method='POST' action='/update' enctype='multipart/form-data'>
+        <input type='file' name='update'>
+        <input type='submit' value='Do it'>
+      </form>
+    )";
+
+    server.send(200, "text/html", content);
+  });
+  // handler for the /update form POST (once file upload finishes)
+  server.on("/update", HTTP_POST, [&](){
+    server.sendHeader("Connection", "close");
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "text/plain", (Update.hasError())?"FAIL":"OK");
+  },[&](){
+    // handler for the file upload, get's the sketch bytes, and writes
+    // them through the Update object
+    HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+    Serial.print("handleFileUpload Name: "); Serial.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
+    filename = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize); // Write the received bytes to the file
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile) {                                    // If the file was successfully created
+      fsUploadFile.close();                               // Close the file again
+      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      server.sendHeader("Location","/success.html");      // Redirect the client to the success page
+      server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
+  }
+    delay(0);
+  });
+  server.on("/style.css", [&](){
+    server.send(200, "text/css",R"(
+      html {
+        font-family:sans-serif;
+        background-color:black;
+        color: #e0e0e0;
+      }
+      div {
+        background-color: #202020;
+      }
+      h1,h2,h3,h4,h5 {
+        color: #e02020;
+      }
+      a {
+        color: #f05050;
+        margin-left:12px;
+      }
+      form *, button {
+        display:block;
+        border: 1px solid #000;
+        font-size: 14px;
+        color: #fff;
+        background: #444;
+        padding: 5px;
+        margin-bottom:12px;
+      }
+      #color-buttons {
+        display:table;
+        max-width:144px;
+      }
+      .color-button {
+        width:36px;
+        height:36px;
+        display:inline;
+        margin:0px !important;
+      }
+    )");
+  });
+  server.on("/", [&](){
+//    if (captivePortal()) { // If caprive portal redirect instead of displaying the page.
+   //   return;
+  //  }
+    server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Expires", "-1");
+
+    String content = header;
+
+  
+    content += R"(
+      </select>
+      <!-- <button type="submit">Set</button> -->
+     <h4>Tag 1</h4>
+      <form method="POST" id="uid1rec" action="/uid1rec">
+        <input name="name" placeholder="Not Set" value=")" + spiffsRead("/uid1str") + R"(">
+        <button type="submit">Register</button>
+      </form>      
+        
+      <h4>URL 1</h4>
+      <form method='POST' id='url1rec' action='/url1rec'>
+        <input name="url1rec" placeholder="Not Set" value=")" + spiffsRead("/url1str") + R"(">
+        <button type='submit'>Save</button>
+      </form>
+     
+             
+    )";
+    server.send(200, "text/html", content);
+  });  
+  server.on("/uid1rec", HTTP_POST, [&](){
+
+      uid1record();
+     send302("/");
+
+});
+  server.on("/url1rec", HTTP_POST, [&](){
+  
+    spiffsWrite("/url1str",server.arg("url1rec"));
+
+      //url1record();
+     send302("/");
+
+});
+
+  
+  server.on("/color", HTTP_PUT, [&](){
+ //   state.color.r = server.arg("r").toInt();
+   // state.color.g = server.arg("g").toInt();
+   // state.color.b = server.arg("b").toInt();
+   // rotateColorFromPalette = ( ( server.arg("rotate").length() > 0 ) ? true : false );
+    
+    server.send(200, "text/plain", "");
+    server.client().stop();
+  });
+ 
+  
+ 
+  server.on("/setup", [&](){
+    server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+    server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    server.sendHeader("Pragma", "no-cache");
+    server.sendHeader("Expires", "-1");
+    server.sendHeader("Content-Length", "-1");
+    server.send(200, "text/html", header);
+    
+    server.sendContent("\
+      <h1>Setup</h1>\
+      <h4>Nearby networks</h4>\
+      <table>\
+      <tr><th>Name</th><th>Security</th><th>Signal</th></tr>\
+    ");
+    Serial.println("[httpd] scan start");
+    int n = WiFi.scanNetworks();
+    Serial.println("[httpd] scan done");
+    for (int i = 0; i < n; i++) {
+      server.sendContent(String() + "\r\n<tr onclick=\"document.getElementById('ssidinput').value=this.firstChild.firstChild.innerHTML; setTimeout(function(){ document.getElementById('pskinput').focus(); }, 100);\"><td><a href=\"#setup-wifi\">" + WiFi.SSID(i) + "</a></td><td>" + String((WiFi.encryptionType(i) == ENC_TYPE_NONE)?"Open":"Encrypted") + "</td><td>" + WiFi.RSSI(i) + "dBm</td></tr>");
+    }
+    server.sendContent(String() + "\
+      </table>\
+      <h4>Connect to a network</h4>\
+      <form method='POST' id='setup-wifi' action='/setup/wifi'>\
+        <input type='text' id='ssidinput' placeholder='network' value='" + String(WiFi.SSID()) + "' name='n'>\
+        <input type='password' id='pskinput' placeholder='password' value='" + String(WiFi.psk()) + "' name='p'>\
+        <button type='submit'>Save and Connect</button>\
+      </form>\
+    ");
+
+    server.sendContent(String() + R"(
+      <h4>Device setup</h4>
+      <form method="POST" id="setup-device" action="/setup/device">
+        <input name="name" placeholder="Device name" value=")" + spiffsRead("/name") + R"(">
+        <button type="submit">Save</button>
+      </form>
+    )");
+
+    server.client().stop();
+  });
+  
+  server.on("/setup/device", HTTP_POST, [&](){
+    Serial.print("[httpd] Device settings post. ");
+    spiffsWrite("/name", server.arg("name"));
+    
+    send302("/setup?saved");
+    Serial.println("done.");
+  });
+  server.on("/debug", [&](){
+    String content = header;
+    content += ("<h1>About</h1><ul>");
+
+    unsigned long uptime = millis();
+    content += (String("<li>Version ") + VERSION + "</li>");
+    content += (String("<li>Booted about ") + (uptime/60000) + " minutes ago (" + ESP.getResetReason() + ")</li>");
+    content += ("</ul>");
+  
+    content += (R"(
+      <h2>Debugging buttons (don't touch)</h2>
+
+      <form method='POST' action='/debug/reset'>
+        <button type='submit'>Restart</button>
+      </form>
+    )");
+    server.send(200, "text/html", content);
+  });
+ /* server.on("/debug/lowpowermode", [&](){
+    //send302("/debug?done");
+   // state.lowPowerMode = !(state.lowPowerMode);
+  });*/
+  server.on("/debug/reset", [&](){
+    send302("/debug?done");
+//    doRestartDevice = true;
+  });
+ // server.on("/debug/sleepforever", [&](){
+  //  send302("/debug?done");    
+ // });
+ // server.on("/debug/disconnect", [&](){
+ //   send302("/debug?done");
+ //   delay(500);
+ //   WiFi.disconnect();
+ // });
+  server.on("/debug/test", [&](){
+   /*  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+    server.send(200, "text/html", "Testing stuff... ");
+
+   uint32_t flashChipSize = ESP.getFlashChipSize();
+   String platform = "generic";
+    if ( flashChipSize == 4194304 ) {
+      platform = "nodemcu"; //HACK HACK HACK
+    }
+    String url = String("http://update.pixilic.com/hoopla.ino.") + platform + ".bin";
+    String versionHeader = String("{\"flashChipSize\":") + String("ghjk") + ", \"version\":" + String("hjk") + ", \"name\":\"" + String("devHostName") + "\"}";
+    server.sendContent(url);
+    server.sendContent(versionHeader);
+
+    server.sendContent("done.");
+    server.client().stop();
+    */
+  });
+  server.on("/version.json", [&](){
+    server.send(200, "text/html", String("1"));
+    server.client().stop();
+  });
+//  server.onNotFound ( handleNotFound );
+  server.begin(); // Web server start
+
+/*
   server.on("/", [](){  
     Serial.println("You called root page");
     server.send(200,"text/html",WebPage);
@@ -174,7 +433,7 @@ void setup()
       UpdateWebPage();
       server.send(200, "text/html", WebPage);
       });
-  server.begin();
+  server.begin();*/
   WiFiManager wifiManager;
   wifiManager.autoConnect("NFC_WiFi");
 
@@ -194,16 +453,7 @@ void loop() {
 }
 
 
-void url1submit()
-{
- notice=server.arg("myText");
- Serial.println("Text Received, contents:");
- Serial.println(notice);
- if (notice != ""){
-  url1str = notice;
-  spiffsWrite("/url1str", notice);
- }
-}
+
 
 void uid1record(){
   LED_Blue();
@@ -253,9 +503,6 @@ void UseURL1(String url1str)
 
 }
 
-void UpdateWebPage(){
-    WebPage = MAIN_page1+uid1str+MAIN_page2+url1str+MAIN_page3;
-}
 
 void nfcread(){
   while(true){
@@ -435,7 +682,11 @@ void LED_Off()
   ledStrip.sendColor(0,0,0,1);
   ledStrip.endFrame(1); 
 }
-
+void send302(String dest) {
+  server.sendHeader("Location", dest, true);
+  server.send ( 302, "text/plain", "");
+  server.client().stop();
+}
 void setClock() {
   configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
   Serial.print("Waiting for NTP time sync: ");
